0f6f917bf01591bb34cbe54aa359c8211b8cdab7
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-01 15:52:54 +0100

    working on diffusive regularizer

--- Image Registration Experiments/cell_centered_grid.m
+++ Image Registration Experiments/cell_centered_grid.m
@@ -6,7 +6,7 @@ function [x, y] = cell_centered_grid(s, h)
 %   x ~ m x n       x-coordinates of grid points
 %   y ~ m x n       y-coordinates of grid points
 
-m = s(1);   
+m = s(1);
 n = s(2);
 
 x = repmat((h(1) / 2) : h(1) : (n - (1 / 2)) * h(1), [m, 1]);
