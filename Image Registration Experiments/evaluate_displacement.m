fc64819ad6515c555f24035a00598efae8bf90d2
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-08 15:04:04 +0100

    derivation routine and format changed

--- Image Registration Experiments/evaluate_displacement.m
+++ Image Registration Experiments/evaluate_displacement.m
@@ -1,12 +1,12 @@
 function [X_u, dX_u] = evaluate_displacement(X, h, u)
 % IN:
-%   X ~ m x n           image
-%   h ~ 2 x 1           grid width
-%   u ~ (m*n) x 2       displacement field [u_x, u_y]
+%   X       ~ m x n             image
+%   h       ~ 2 x 1             grid width
+%   u       ~ (m*n) x 2         displacement field [u_x, u_y]
 % OUT:
-%   X_u ~ m x n         interpolation of X over grid displaced by u
-%   dX_u ~ m x n x 2    interpolated gradient of X over displaced grid
-    
+%   X_u     ~ m x n             interpolation of X over grid displaced by u
+%   dX_u    ~ (m*n) x (m*n*2)   gradient of interpol(X) over displaced grid
+
 [m, n] = size(X);
 
 % get cell centered grid
@@ -17,26 +17,13 @@ p = [x(:), y(:)];
 p_displaced = p + u;
 
 % interpolate X over resulting grid
-X_u = bilinear_interpolation(X, h, p_displaced);
-X_u = reshape(X_u, [m, n]);
-
-% return gradient of interpolated image (if requested)
-if (nargout == 2)
-    
-    % compute image gradient of X only once, save as persistent variables
-    persistent dx;  persistent dy;
-    if isempty(dx)
-        [dx, dy] = image_gradient(X, h);
-    end
-    
-    % interpolate dX_u/dx and dX_u/dy from dX/dx and dX_/dy
-    dX_u_x = bilinear_interpolation(dx, h, p_displaced);
-    dX_u_y = bilinear_interpolation(dy, h, p_displaced);
-    
-    dX_u = zeros(m, n, 2);
-    dX_u(:, :, 1) = reshape(dX_u_x, [m, n]);
-    dX_u(:, :, 2) = reshape(dX_u_y, [m, n]);
-    
+if nargout == 1
+    X_u = bilinear_interpolation(X, h, p_displaced);
+elseif nargout == 2
+    [X_u, dX_u] = bilinear_interpolation(X, h, p_displaced);
+    dX_u = [spdiags(dX_u(:, 1), 0, m*n, m*n), ...
+        spdiags(dX_u(:, 2), 0, m*n, m*n)];
 end
+X_u = reshape(X_u, [m, n]);
 
 end
