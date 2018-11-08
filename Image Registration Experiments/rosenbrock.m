5fb05952199ed5483e121e3140d9e07e0bdb050f
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-08 15:11:09 +0100

    Hessian added

--- Image Registration Experiments/rosenbrock.m
+++ Image Registration Experiments/rosenbrock.m
@@ -1,12 +1,15 @@
-function [f, df] = rosenbrock(x)
+function [f, df, d2f] = rosenbrock(x)
 % IN:
-%   x ~ 2 x 1           evaluation point
+%   x   ~ 2 x 1         evaluation point
 % OUT:
-%   f  ~ 1 x 1          function value f(x)
-%   df ~ 2 x 1          gradient of f at x
+%   f   ~ 1 x 1         function value f(x)
+%   df  ~ 2 x 1         gradient of f at x
+%   d2f ~ 2 x 2         Hessian of f at x
 
 f = (1 - x(1)) ^ 2 + 100 * (x(2) - x(1) ^ 2) ^ 2;
 df = [(-2) * (1 - x(1)) - 400 * x(1) * (x(2) - x(1) ^ 2); ...
     200 * (x(2) - x(1) ^ 2)];
+d2f = [2 + 800 * x(1) ^ 2 - 400 * (x(2) - x(1) ^ 2), (-400) * x(1); ...
+    (-400) * x(1), 200];
 
 end
