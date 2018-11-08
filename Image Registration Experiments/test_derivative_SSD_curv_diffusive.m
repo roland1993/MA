54e9d16759ec337bf909d64af3491e8b8585411e
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-08 15:14:08 +0100

    test script for testing derivatives of SSD/CURV/DIFFUSIVE

---
+++ Image Registration Experiments/test_derivative_SSD_curv_diffusive.m
@@ -0,0 +1,22 @@
+%% initialization
+clear all, close all, clc;
+
+R = double(imread('hand1.png'));
+T = double(imread('hand2.png'));
+h = [1, 1];
+s = size(R);
+
+% randomize displacement field
+u0 = randn(2 * prod(s), 1);
+
+%% test SSD derivative
+SSD_handle = @(u) SSD(T, R, h, u);
+derivative_test(SSD_handle, u0, 1);
+
+%% test curvature_energy derivative
+curv_handle = @(u) curvature_energy(u, s, h);
+derivative_test(curv_handle, u0);
+
+%% test diffusive_energy derivative
+diff_handle = @(u) diffusive_energy(u, s, h);
+derivative_test(diff_handle, u0);
