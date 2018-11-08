f42ead330177fc37cdbd5835137444b8af6eb4a3
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-08 14:59:16 +0100

    x-/y-derivatives of interpolation model added

--- Image Registration Experiments/bilinear_interpolation.m
+++ Image Registration Experiments/bilinear_interpolation.m
@@ -1,10 +1,11 @@
-function X_p = bilinear_interpolation(X, h, p)
+function [X_p, dX_p] = bilinear_interpolation(X, h, p)
 % IN:
-%   X ~ m x n       image
-%   h ~ 2 x 1       grid width [h_x, h_y]
-%   p ~ l x 2       evaluation points
+%   X       ~ m x n     image
+%   h       ~ 2 x 1     grid width [h_x, h_y]
+%   p       ~ l x 2     evaluation points
 % OUT:
-%   X_p ~ l x 1     bilinear interpolation of X at points p
+%   X_p     ~ l x 1     bilinear interpolation of X at points p
+%   dX_p    ~ l x 2     x- and y-derivative of interpol(X) at points p
 
 [m, n] = size(X);
 l = size(p, 1);
@@ -21,12 +22,22 @@ Y = padarray(X, [1 1]);
 
 % evaluate linear interpolation of X at given points p
 X_p = zeros(l, 1);
+
+% evaluate x- and y-derivative of interpol(X) if requested
+if nargout == 2
+    dX_p = zeros(l, 2);
+end
+
 for i = 1 : l
     
-    % set interpolation value to 0 outside of [0, m] x [0, n]
+    % set interpolation value to 0 for (i, j) not in [0, m+1] x [0, n+1]
+    %   <=> (x, y) not in [0, n*h1] x [0, m*h2]
     if (u(i, 1) <= 0) || (u(i, 1) >= (m + 1)) || ...
             (u(i, 2) <= 0) || (u(i, 2) >= (n + 1))
         X_p(i) = 0;
+        if nargout == 2
+            dX_p(i, :) = [0, 0];
+        end
     else
         int = floor(u(i, :));
         dec = u(i, :) - int;
@@ -36,6 +47,21 @@ for i = 1 : l
             dec(1) * (1 - dec(2)) * Y(int(1) + 2, int(2) + 1) + ...
             (1 - dec(1)) * dec(2) * Y(int(1) + 1, int(2) + 2) + ...
             dec(1) * dec(2) * Y(int(1) + 2, int(2) + 2);
+        
+        if nargout == 2
+            % x-derivative
+            dX_p(i, 1) = (1 / h(1)) * ...
+                (1 - dec(1)) * (-1) * Y(int(1) + 1, int(2) + 1) + ...
+                dec(1) * (-1) * Y(int(1) + 2, int(2) + 1) + ...
+                (1 - dec(1)) * (+1) * Y(int(1) + 1, int(2) + 2) + ...
+                dec(1) * (+1) * Y(int(1) + 2, int(2) + 2);
+            % y-derivative
+            dX_p(i, 2) = (1 / h(2)) * ...
+                (+1) * (1 - dec(2)) * Y(int(1) + 1, int(2) + 1) + ...
+                (-1) * (1 - dec(2)) * Y(int(1) + 2, int(2) + 1) + ...
+                (+1) * dec(2) * Y(int(1) + 1, int(2) + 2) + ...
+                (-1) * dec(2) * Y(int(1) + 2, int(2) + 2);
+        end
     end
     
 end
