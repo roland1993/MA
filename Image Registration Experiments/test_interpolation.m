e3d5bcec1c7bfbdc6a64288a8b65d796a4bfc451
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-13 12:55:31 +0100

    changed naming convention of image variables

--- Image Registration Experiments/test_interpolation.m
+++ Image Registration Experiments/test_interpolation.m
@@ -5,7 +5,7 @@ clear all, close all, clc;
 m = 3;
 n = 4;
 h = [1, 1];
-X = randi(256, m, n) - 1;
+img = randi(256, m, n) - 1;
 
 %% display image data
 figure('units', 'normalized', 'outerposition', [0 0 1 1]);
@@ -14,7 +14,7 @@ hold on;
 image(...
     'Xdata', [h(1) / 2, (n - (1 / 2)) * h(1)], ...
     'YData', [h(2) / 2, (m - (1 / 2)) * h(2)], ...
-    'CData', flipud(X));
+    'CData', flipud(img));
 axis xy;
 axis image;
 colorbar;
@@ -25,17 +25,17 @@ ylabel('---y-->');
 [x, y] = cell_centered_grid([m, n], h);
 p = [x(:), y(:)];
 plot_grid(reshape(p, [m, n, 2]));
-title('image X with cell centered grid');
+title('image img with cell centered grid');
 
-%% interpolate X over cell centered grid (... should yield X)
-X_interpol = bilinear_interpolation(X, h, p);
-X_interpol = reshape(X_interpol, [m, n]);
-fprintf('||X - X_interpol|| = %.3e\n', norm(X(:) - X_interpol(:)));
+%% interpolate img over cell centered grid (... should yield img)
+img_interpol = bilinear_interpolation(img, h, p);
+img_interpol = reshape(img_interpol, [m, n]);
+fprintf('||img - img_interpol|| = %.3e\n', norm(img(:) - img_interpol(:)));
 
 %% randomize small displacement to test evaluate_displacement
 u = 0.1 * randn(m * n, 2);
 p_displaced = p + u;
-X_u = evaluate_displacement(X, h, u);
+img_u = evaluate_displacement(img, h, u);
 
 % display results
 figure('units', 'normalized', 'outerposition', [0 0 1 1]);
@@ -46,23 +46,23 @@ hold on;
 image(...
     'Xdata', [h(1) / 2, (n - (1 / 2)) * h(1)], ...
     'YData', [h(2) / 2, (m - (1 / 2)) * h(2)], ...
-    'CData', flipud(X));
+    'CData', flipud(img));
 axis xy;
 axis image;
 colorbar;
 xlabel('---x-->');
 ylabel('---y-->');
 plot_grid(reshape(p_displaced, [m, n, 2]));
-title('image X with displaced grid');
+title('image img with displaced grid');
 
 subplot(1, 2, 2);
 image(...
     'Xdata', [h(1) / 2, (n - (1 / 2)) * h(1)], ...
     'YData', [h(2) / 2, (m - (1 / 2)) * h(2)], ...
-    'CData', flipud(X_u));
+    'CData', flipud(img_u));
 axis xy;
 axis image;
 colorbar;
 xlabel('---x-->');
 ylabel('---y-->');
-title('image X interpolated at displaced grid');
+title('image img interpolated at displaced grid');
