272a53ae161aa2e997260388b2f0ffecc9417ed1
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-14 11:09:51 +0100

    coordinate system changed

--- Image Registration Experiments/gradient_operator.m
+++ Image Registration Experiments/gradient_operator.m
@@ -13,24 +13,24 @@ function [GXtGX, GYtGY] = gradient_operator(s, h)
 m = s(1);   n = s(2);
 
 % 1. finite x-differences (evaluated over x-staggered grid)
-e = ones(n, 1);
-D = (1 / h(1)) * spdiags([-e, e], -1 : 0, n + 1, n);
+e = ones(m, 1);
+D = (1 / h(1)) * spdiags([-e, e], -1 : 0, m + 1, m);
 D(1, 1) = 0;    D(end, end) = 0;        % Neumann boundary condition
-Dx = kron(D, speye(m));
+Dx = kron(speye(n), D);
 
 % 2. finite y-differences (evaluated over y-staggered grid)
-e = ones(m, 1);
-D = (1 / h(2)) * spdiags([e, -e], -1 : 0, m + 1, m);
+e = ones(n, 1);
+D = (1 / h(2)) * spdiags([-e, e], -1 : 0, n + 1, n);
 D(1, 1) = 0;    D(end, end) = 0;        % Neumann boundary condition
-Dy = kron(speye(n), D);
+Dy = kron(D, speye(m));
 
 % 3. averaging in x-direction
-ax = [1/2; ones(n - 1, 1); 1/2];
-Ax = spdiags(kron(ax, ones(m, 1)), 0, (n + 1) * m, (n + 1) * m);
+ax = [1/2; ones(m - 1, 1); 1/2];
+Ax = spdiags(kron(ones(n, 1), ax), 0, (m + 1) * n, (m + 1) * n);
 
 % 4. averaging in y-direction
-ay = [1/2; ones(m - 1, 1); 1/2];
-Ay = spdiags(kron(ones(n, 1), ay), 0, n * (m + 1), n * (m + 1));
+ay = [1/2; ones(n - 1, 1); 1/2];
+Ay = spdiags(kron(ay, ones(m, 1)), 0, m * (n + 1), m * (n + 1));
 
 % combine to form output
 GXtGX = Dx' * Ax * Dx;
