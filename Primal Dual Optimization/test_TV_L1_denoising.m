676b9788b496e659a0034bcd685e5a75f781582b
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-21 16:28:25 +0100

    minor changes

--- Primal Dual Optimization/test_TV_L1_denoising.m
+++ Primal Dual Optimization/test_TV_L1_denoising.m
@@ -5,6 +5,7 @@ clear all, close all, clc;
 
 % read sample image
 img = double(imread('westconcordorthophoto.png'));
+% img = double(imread('louvre.jpg'));
 
 % normalize image to [0, 1]
 img = (img - min(img(:))) / (max(img(:)) - min(img(:)));
@@ -16,30 +17,6 @@ img_noisy = imnoise(img, 'salt & pepper', p);
 % get image size & set grid width / height
 [m, n] = size(img);
 h = [1, 1];
-
-% display input data
-figure('units', 'normalized', 'outerposition', [0 0 1 1]);
-colormap gray(256);
-
-subplot(2, 2, 1);
-imagesc(...
-    'YData', [h(1) * (1/2), h(1) * (m - (1/2))], ...
-    'XData', [h(2) * (1/2), h(2) * (n - (1/2))], ...
-    'CData', img);
-axis image;     set(gca, 'YDir', 'reverse');
-colorbar;
-xlabel('---y-->');      ylabel('<--x---');
-title('input image img');
-
-subplot(2, 2, 2);
-imagesc(...
-    'YData', [h(1) * (1/2), h(1) * (m - (1/2))], ...
-    'XData', [h(2) * (1/2), h(2) * (n - (1/2))], ...
-    'CData', img_noisy);
-axis image;     set(gca, 'YDir', 'reverse');
-colorbar;
-xlabel('---y-->');      ylabel('<--x---');
-title(['img\_noisy with ', num2str(p * 100),'% salt & pepper noise']);
 
 %% setup and optimization
 
@@ -64,8 +41,8 @@ maxIter = 250;
 
 % function handles for data term and regularizer
 lambda = 1.75;
-G = @(u) SAD(u, img_noisy(:), lambda, tau);
-F = @(v) TV(v, sigma);
+G = @(u, c_flag) SAD(u, img_noisy(:), lambda, tau, c_flag);
+F = @(v, c_flag) TV(v, sigma, c_flag);
 
 % perform optimization
 [u_star, v_star] = ...
@@ -74,6 +51,31 @@ img_denoise = reshape(u_star, size(img));
 
 %% display results
 
+figure('units', 'normalized', 'outerposition', [0 0 1 1]);
+colormap gray(256);
+
+% input data
+subplot(2, 2, 1);
+imagesc(...
+    'YData', [h(1) * (1/2), h(1) * (m - (1/2))], ...
+    'XData', [h(2) * (1/2), h(2) * (n - (1/2))], ...
+    'CData', img);
+axis image;     set(gca, 'YDir', 'reverse');
+colorbar;
+xlabel('---y-->');      ylabel('<--x---');
+title('input image img');
+
+subplot(2, 2, 2);
+imagesc(...
+    'YData', [h(1) * (1/2), h(1) * (m - (1/2))], ...
+    'XData', [h(2) * (1/2), h(2) * (n - (1/2))], ...
+    'CData', img_noisy);
+axis image;     set(gca, 'YDir', 'reverse');
+colorbar;
+xlabel('---y-->');      ylabel('<--x---');
+title(['img\_noisy with ', num2str(p * 100),'% salt & pepper noise']);
+
+% output data
 subplot(2, 2, 3);
 imagesc(...
     'YData', [h(1) * (1/2), h(1) * (m - (1/2))], ...
