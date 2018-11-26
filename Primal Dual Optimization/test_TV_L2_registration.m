829998769dab030bc0694455cedbd8c539f5631d
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-26 14:24:19 +0100

    minor changes

--- Primal Dual Optimization/test_TV_L2_registration.m
+++ Primal Dual Optimization/test_TV_L2_registration.m
@@ -13,8 +13,8 @@ if exist('evaluate_displacement.m') == 0
 end
 
 % read reference and template images
-R = double(imread('rect1.png'));
-T = double(imread('rect2.png'));
+R = double(imread('rect3.png'));
+T = double(imread('rect4.png'));
 
 % normalize images
 R = (R - min(R(:))) / (max(R(:)) - min(R(:)));
@@ -26,7 +26,7 @@ h = [1, 1];
 
 %% setup and optimization
 
-lambda = 10;
+lambda = 0.01;
 
 % define discrete gradient operator K
 Dx = (1 / h(1)) * spdiags([-ones(m, 1), ones(m, 1)], 0 : 1, m, m);
@@ -43,7 +43,7 @@ L_squared = 4 * lambda ^ 2 * (1 / h(1) ^ 2 + 1 / h(2) ^ 2);
 u0 = zeros(m * n * 2, 1);
 v0 = zeros(m * n * 4, 1);
 theta = 1;
-tau = 10;
+tau = 100;
 sigma = 1 / (L_squared * tau);
 maxIter = 30;
 
@@ -51,20 +51,20 @@ maxIter = 30;
 G = @(u, c_flag) SSD_registration(u, u0, T, R, h, tau, c_flag);
 F = @(v, c_flag) TV_registration(v, sigma, c_flag);
 
-% figure('units', 'normalized', 'outerposition', [0 0 1 1]);
-% colormap gray(256);
+figure('units', 'normalized', 'outerposition', [0 0 1 1]);
+colormap gray(256);
 
 % perform optimization
 for i = 1 : 40
     
-%     T_star = evaluate_displacement(T, h, reshape(u0, [], 2));
-%     imagesc(...
-%         'YData', [h(1) * (1/2), h(1) * (m - (1/2))], ...
-%         'XData', [h(2) * (1/2), h(2) * (n - (1/2))], ...
-%         'CData', T_star);
-%     axis image;     set(gca, 'YDir', 'reverse');
-%     drawnow;
-%     title(sprintf('i = %d', i));
+    T_star = evaluate_displacement(T, h, reshape(u0, [], 2));
+    imagesc(...
+        'YData', [h(1) * (1/2), h(1) * (m - (1/2))], ...
+        'XData', [h(2) * (1/2), h(2) * (n - (1/2))], ...
+        'CData', T_star);
+    axis image;     set(gca, 'YDir', 'reverse');
+    drawnow;
+    title(sprintf('i = %d', i));
     
     [u_star, v_star] = chambolle_pock(F, G, K, u0, v0, theta, tau, ...
         sigma, maxIter, -1, -1, -1);
