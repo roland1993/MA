6f3f9bc0ee2689731dd1184a4a57189f1ad90d73
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-29 21:15:17 +0100

    added function value of conjugate

--- Primal Dual Optimization/SSD_registration.m
+++ Primal Dual Optimization/SSD_registration.m
@@ -29,12 +29,12 @@ k = numel(T);
 
 % linearize interpolation of T at u0
 [T, grad_T] = evaluate_displacement(T, h, reshape(u0, k, 2));
+b = T(:) - (grad_T * u0) - R(:);
 
 if ~conjugate_flag
     % EITHER ~> evaluate SSD and Prox_[SSD] at u
     
     % SSD = 0.5 * ||grad_T(u0) * u + b||^2, b = T(u0) - grad_T(u0)*u0 - R
-    b = T(:) - (grad_T * u0) - R(:);
     residual = (grad_T * u) + b;
     res1 = (1 / 2) * (residual' * residual);
     
@@ -53,8 +53,41 @@ if ~conjugate_flag
     end
     
 else
-    % ... todo
-    res1 = -inf;
+    
+    % conjugate of SSD = sum_ij 0.5 * (grad_T_ij * u_ij + b_ij) ^ 2
+    %   = sum_ij {0.5 * u_ij' * grad_T_ij' * grad_T_ij * u_ij
+    %       + u_ij' * grad_T_ij' * b_ij
+    %       + 0.5 * b_ij ^ 2}
+    % computed as pointwise conjugates of a quadratic model
+    %   0.5 * x' * A * x + x' * b + c
+    % ~> see Rockafellar, page 481
+    grad_T = spdiags(grad_T);
+    norm_grad_squared = sum(grad_T .^ 2, 2);
+    idx = (norm_grad_squared > 1e-7);
+    
+    u = reshape(u, [], 2);
+    res1 = 0.5 * sum((u - b .* grad_T) .* grad_T, 2) .^ 2;
+    res1(idx) = res1(idx) ./ (norm_grad_squared(idx) .^ 2);
+    
+    res1 = res1 - 0.5 * b .^ 2;
+    res1 = sum(res1);
+    
+    % constraint: (v - b) has to be in Image(pinv(A))
+    %   -> error measure given by distance from this subspace
+    res3 = zeros(size(idx));
+    
+    % case 1: grad_T_ij = 0
+    %   -> error measure is distance from 0
+    res3(~idx) = sqrt(sum(u(~idx, :) .^ 2, 2));
+    
+    % case 2: grad_T_ij ~= 0
+    %   -> error measure is distance to subspace {mu * grad_T_ij}
+    grad_T(idx, :) = grad_T(idx, :) ./ sqrt(norm_grad_squared(idx));
+    grad_T_orth = [-grad_T(:, 2), grad_T(:, 1)];
+    res3(idx) = abs(sum(grad_T_orth(idx, :) .* u(idx, :), 2));
+    
+    % return maximum error over all ij
+    res3 = max(res3);
     
     % Prox_[SSD*]
     if nargout == 2
