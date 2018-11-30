6a2a76e8f9d675c29f992778cbf84798ade90cf8
roland1993 <roland.haase@student.uni-luebeck.de>
2018-11-30 09:38:30 +0100

    incorporated test functions into test script to reduce file overhead

--- Primal Dual Optimization/test_chambolle_pock.m
+++ Primal Dual Optimization/test_chambolle_pock.m
@@ -35,3 +35,31 @@ plot(1 : numel(primal_history), primal_history, ...
     1 : numel(primal_history), dual_history);
 grid on;    axis tight;
 legend('primal energy', 'dual energy');     xlabel('#iter');
+
+%% F and G as local functions inside script
+
+function [res1, res2, res3] = test_F(y, sigma, conjugate_flag)
+
+res3 = 0;
+if ~conjugate_flag
+    res1 = 0.5 * sum(y .^ 2);
+    res2 = 1 / (1 + sigma) * y;
+else
+    res1 = 0.5 * sum(y .^ 2);
+    res2 = 1 / (1 + sigma) * y;
+end
+
+end
+
+function [res1, res2, res3] = test_G(x, g, lambda, tau, conjugate_flag)
+
+res3 = 0;
+if ~conjugate_flag
+    res1 = 0.5 * lambda * sum((x(:) - g(:)) .^ 2);
+    res2 = 1 / (1 + (lambda * tau)) * (x + (lambda * tau) * g);
+else
+    res1 = lambda * (0.5 * sum((x / lambda) .^ 2) + ((x / lambda)' * g));
+    res2 = [];
+end
+
+end
