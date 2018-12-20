3c99335c56eea541c17d4ecfcf58f7913f4ab952
roland1993 <roland.haase@student.uni-luebeck.de>
2018-12-20 14:59:10 +0100

    added support for arbitrary evaluation regions omega

--- Image Registration Experiments/evaluate_displacement.m
+++ Image Registration Experiments/evaluate_displacement.m
@@ -1,29 +1,40 @@
-function [img_u, dimg_u] = evaluate_displacement(img, h, u)
+function [img_u, dimg_u] = evaluate_displacement(img, h, u, omega, s_grid)
 % IN:
 %   img     ~ m x n             image
-%   h       ~ 2 x 1             grid width
-%   u       ~ (m*n) x 2         displacement field [u_x, u_y]
+%   h       ~ 2 x 1             image step sizes [h_x, h_y]
+%   u       ~ (k*l) x 2         displacement field [u_x, u_y]
+%   omega   ~ 1 x 4             evaluation region
+%                                   [omega_1, omega_2] x [omega_3, omega_4]
+%   s_grid  ~ 2 x 1             evaluation grid resolution = [k, l]
 % OUT:
-%   img_u   ~ m x n             interpolation of img over displaced grid
-%   dimg_u  ~ (m*n) x (m*n*2)   gradient of interpol(img)
+%   img_u   ~ k x l             interpolation of img over displaced grid
+%   dimg_u  ~ (k*l) x (k*l*2)   gradient of interpol(img)
 
+% get image resolution
 [m, n] = size(img);
 
-% get cell centered grid
-[x, y] = cell_centered_grid([m, n], h);
+% default grid resolution = image resolution
+if nargin < 5
+    s_grid = [m, n];
+end
+k = s_grid(1);      l = s_grid(2);
+
+% default image region = [0, m * h_x] x [0, n * h_y]
+if nargin < 4
+    omega = [0, m * h(1), 0, n * h(2)];
+end
+
+% get cell centered grid over omega with resolution s_grid
+[x, y] = cell_centered_grid(omega, [k, l]);
 p = [x(:), y(:)];
 
 % displace grid by u
 p_displaced = p + u;
 
 % interpolate img over resulting grid
-if nargout == 1
-    img_u = bilinear_interpolation(img, h, p_displaced);
-elseif nargout == 2
-    [img_u, dimg_u] = bilinear_interpolation(img, h, p_displaced);
-    dimg_u = [spdiags(dimg_u(:, 1), 0, m*n, m*n), ...
-        spdiags(dimg_u(:, 2), 0, m*n, m*n)];
-end
-img_u = reshape(img_u, [m, n]);
+[img_u, dimg_u] = bilinear_interpolation(img, h, p_displaced);
+dimg_u = [spdiags(dimg_u(:, 1), 0, k*l, k*l), ...
+    spdiags(dimg_u(:, 2), 0, k*l, k*l)];
+img_u = reshape(img_u, [k, l]);
 
 end
