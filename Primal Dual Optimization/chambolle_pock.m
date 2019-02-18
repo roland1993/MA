b893d36183d585aacaaad6a1c86de6804d807ef3
roland1993 <roland.haase@student.uni-luebeck.de>
2019-02-18 14:26:28 +0100

    more robust stopping criterion + support for vector-valued F and G

--- Primal Dual Optimization/chambolle_pock.m
+++ Primal Dual Optimization/chambolle_pock.m
@@ -65,29 +65,51 @@ Kt = K';
 % initialize iteration counter
 i = 0;
 
+% track numerical primal-dual gap
+GAP = zeros(maxIter + 1, 1);
+
 % initialize iteration variables
 y_current = y0;
 x_current = x0;
 x_bar = x0;
 
 % record progress in primal, F and G ...
-primal_history = zeros(maxIter + 1, 1);
-G_history = zeros(maxIter + 1, 1);
-F_history = zeros(maxIter + 1, 1);
 F_con = zeros(maxIter + 1, 1);
 G_con = zeros(maxIter + 1, 1);
-[F_history(1), G_history(1), F_con(1), G_con(1)] = primal_objective(x0);
-primal_history(1) = G_history(1) + F_history(1);
+[F_history, G_history, F_con(1), G_con(1)] = primal_objective(x0);
+primal_history = zeros(maxIter + 1, 1);
+primal_history(1) = sum(G_history(1, :)) + sum(F_history(1, :));
+F_history = [F_history; zeros(maxIter, size(F_history, 2))];
+G_history = [G_history; zeros(maxIter, size(G_history, 2))];
+
+% primal_history = zeros(maxIter + 1, 1);
+% G_history = zeros(maxIter + 1, 1);
+% F_history = zeros(maxIter + 1, 1);
+% F_con = zeros(maxIter + 1, 1);
+% G_con = zeros(maxIter + 1, 1);
+% [F_history(1), G_history(1), F_con(1), G_con(1)] = primal_objective(x0);
+% primal_history(1) = G_history(1) + F_history(1);
 
 % ... as well for dual, F* and G*
-dual_history = zeros(maxIter + 1, 1);
-GStar_history = zeros(maxIter + 1, 1);
-FStar_history = zeros(maxIter + 1, 1);
 FS_con = zeros(maxIter + 1, 1);
 GS_con = zeros(maxIter + 1, 1);
-[FStar_history(1), GStar_history(1), FS_con(1), GS_con(1)] = ...
+[FStar_history, GStar_history, FS_con(1), GS_con(1)] = ...
     dual_objective(y0);
-dual_history(1) = -(GStar_history(1) + FStar_history(1));
+dual_history = zeros(maxIter + 1, 1);
+dual_history(1) = ...
+    -(sum(GStar_history(1, :)) + sum(FStar_history(1, :)));
+FStar_history = [FStar_history; zeros(maxIter, size(FStar_history, 2))];
+GStar_history = [GStar_history; zeros(maxIter, size(GStar_history, 2))];
+
+% dual_history = zeros(maxIter + 1, 1);
+% GStar_history = zeros(maxIter + 1, 1);
+% FStar_history = zeros(maxIter + 1, 1);
+% FS_con = zeros(maxIter + 1, 1);
+% GS_con = zeros(maxIter + 1, 1);
+% [FStar_history(1), GStar_history(1), FS_con(1), GS_con(1)] = ...
+%     dual_objective(y0);
+% dual_history(1) = -(GStar_history(1) + FStar_history(1));
+
 
 % output some info
 fprintf('\nCHAMBOLLE POCK PRIMAL DUAL OPTIMIZATION SCHEME\n');
@@ -123,12 +145,23 @@ while true
     end
     
     % second stopping criterion
-    GAP = abs((primal_history(i + 1) - dual_history(i + 1)) / ...
+    GAP(i + 1) = abs((primal_history(i + 1) - dual_history(i + 1)) / ...
         dual_history(i + 1));
-    if ~isnan(GAP) && max([GAP, F_con(i + 1), G_con(i + 1), ...
-            FS_con(i + 1), GS_con(i + 1)]) <= tol
+    
+    if (i >= 4) && ...
+            ~any(isnan(GAP(i - 3 : i + 1))) && ...
+            all(GAP(i - 3 : i + 1) <= tol) && ...
+            all(F_con(i - 3 : i + 1) <= tol) && ...
+            all(G_con(i - 3 : i + 1) <= tol) && ...
+            all(FS_con(i - 3 : i + 1) <= tol) && ...
+            all(GS_con(i - 3 : i + 1) <= tol)
         break;
     end
+    
+%     if ~isnan(GAP) && max([GAP, F_con(i + 1), G_con(i + 1), ...
+%             FS_con(i + 1), GS_con(i + 1)]) <= tol
+%         break;
+%     end
     
     % increase iteration counter
     i = i + 1;
@@ -143,26 +176,30 @@ while true
     [~, ~, x_current] = G(x_current - tau * (Kt * y_current), false);
     
     % record primal and dual objective value for current iterates
-    [F_history(i + 1), G_history(i + 1), F_con(i + 1), G_con(i + 1)] = ...
-        primal_objective(x_current);
-    primal_history(i + 1) = G_history(i + 1) + F_history(i + 1);
+    [F_history(i + 1, :), G_history(i + 1, :), ...
+        F_con(i + 1), G_con(i + 1)] = primal_objective(x_current);
+    primal_history(i + 1) = ...
+        sum(G_history(i + 1, :)) + sum(F_history(i + 1, :));
     
-    [FStar_history(i + 1), GStar_history(i + 1), FS_con(i + 1), ...
-        GS_con(i + 1)] = dual_objective(y_current);
-    dual_history(i + 1) = -(GStar_history(i + 1) + FStar_history(i + 1));
+    [FStar_history(i + 1, :), GStar_history(i + 1, :), ...
+        FS_con(i + 1), GS_con(i + 1)] = dual_objective(y_current);
+    dual_history(i + 1) = ...
+        -(sum(GStar_history(i + 1, :)) + sum(FStar_history(i + 1, :)));
     
     % x_bar_{n+1} = x_{n+1} + theta * (x_{n+1} - x_n)
     x_bar = x_current + theta * (x_current - x_old);
     
     % iterative output
-    fprintf('%d\t%+.2e\t%+.2e\t%.3e', i, primal_history(i + 1), ...
-        dual_history(i + 1), abs((primal_history(i + 1) - ...
-        dual_history(i + 1)) / dual_history(i + 1)));
-    fprintf('\tF: %.2e', F_con(i + 1));
-    fprintf('\tG: %.2e', G_con(i + 1));
-    fprintf('\tF*: %.2e', FS_con(i + 1));
-    fprintf('\tG*: %.2e', GS_con(i + 1));
-    fprintf('\n');
+    if mod(i, 10) == 0
+        fprintf('%d\t%+.2e\t%+.2e\t%.3e', i, primal_history(i + 1), ...
+            dual_history(i + 1), abs((primal_history(i + 1) - ...
+            dual_history(i + 1)) / dual_history(i + 1)));
+        fprintf('\tF: %.2e', F_con(i + 1));
+        fprintf('\tG: %.2e', G_con(i + 1));
+        fprintf('\tF*: %.2e', FS_con(i + 1));
+        fprintf('\tG*: %.2e', GS_con(i + 1));
+        fprintf('\n');
+    end
     
 end
 
@@ -175,14 +212,14 @@ else
     
     % delete redundant entries
     primal_history(i + 2 : end) = [];
-    F_history(i + 2 : end) = [];
-    G_history(i + 2 : end) = [];
+    F_history(i + 2 : end, :) = [];
+    G_history(i + 2 : end, :) = [];
     F_con(i + 2 : end) = [];
     G_con(i + 2 : end) = [];
     
     dual_history(i + 2 : end) = [];
-    FStar_history(i + 2 : end) = [];
-    GStar_history(i + 2 : end) = [];
+    FStar_history(i + 2 : end, :) = [];
+    GStar_history(i + 2 : end, :) = [];
     FS_con(i + 2 : end) = [];
     GS_con(i + 2 : end) = [];
     
