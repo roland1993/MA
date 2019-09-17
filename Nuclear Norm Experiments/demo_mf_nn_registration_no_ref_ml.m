727b05871a71242ddc87da3ab84b94e842298e33
Roland Haase <roland.haase@mic.uni-luebeck.de>
2019-09-17 16:57:25 +0200

    switch/case for different data sets added

--- Nuclear Norm Experiments/demo_mf_nn_registration_no_ref_ml.m
+++ Nuclear Norm Experiments/demo_mf_nn_registration_no_ref_ml.m
@@ -12,44 +12,92 @@
 % demo script for mf_nn_registration_no_ref_ml.m
 clear all, close all, clc;
 
-% % create data
-% m = 200;    n = 200;    k = 8;
-% data = dynamicTestImage(m, n, k);
-% img = cell(k, 1);
-% for i = 1 : k, img{i} = data(:, :, i); end
-% 
-% % set optimization parameters
-% optPara.theta = 1;
-% optPara.maxIter = 2000;
-% optPara.tol = 1e-3;
-% optPara.outerIter = 15;
-% optPara.mu = 2e-1;
-% optPara.nu_factor = 0.9;
-% optPara.bc = 'linear';
-% optPara.doPlots = true;
+% choose dataset from {synthetic, heart, kidney}
+dataset = 'kidney';
 
-% load data
-load('heart_mri.mat');
-IDX = [3, 10, 17, 32, 39, 45, 60, 66, 73, 90, 96, 104];
-k = length(IDX);
-
-% downsampling
-img = cell(k, 1);
-factor = 2;
-for i = 1 : k
-    img{i} = conv2(data(:, :, IDX(i)), ones(factor) / factor ^ 2, 'same');
-    img{i} = img{i}(1 : factor : end, 1 : factor : end);
+switch dataset
+    case 'synthetic'
+        
+        % generate data
+        m = 200;    n = 200;    k = 8;
+        data = dynamicTestImage(m, n, k);
+        img = cell(k, 1);
+        for i = 1 : k, img{i} = data(:, :, i); end
+        
+        % set optimization parameters
+        optPara.theta = 1;
+        optPara.maxIter = 2000;
+        optPara.tol = 1e-3;
+        optPara.outerIter = [15 2];
+        optPara.mu = 2e-1;
+        optPara.nu_factor = [0.9 0.9];
+        optPara.bc = 'neumann';
+        optPara.doPlots = true;
+        
+    case 'heart'
+        
+        % load data
+        load('heart_mri.mat');
+        IDX = [ 003, 010, 017, ...
+                032, 039, 045, ...
+                060, 066, 073, ...
+                090, 096, 104, ...
+                121, 126, 134, ...
+                154, 159, 167, ...
+                194, 197, 206];
+        k = length(IDX);
+        
+        % downsampling
+        img = cell(k, 1);
+        factor = 2;
+        for i = 1 : k
+            img{i} = conv2(data(:, :, IDX(i)), ...
+                ones(factor) / factor ^ 2, 'same');
+            img{i} = img{i}(1 : factor : end, 1 : factor : end);
+        end
+        
+        % set optimization parameters
+        optPara.theta = 1;
+        optPara.maxIter = 2000;
+        optPara.tol = 1e-3;
+        optPara.outerIter = [16 2];
+        optPara.mu = 1.25e-1;
+        optPara.nu_factor = [0.95 0.95];
+        optPara.bc = 'neumann';
+        optPara.doPlots = true;
+        
+    case 'kidney'
+        
+        % load data
+        load('respfilm1gray.mat');
+        k = 12;
+        IDX = round(linspace(1, size(A, 3)/ 3, k));
+        img = cell(1, k);
+        for i = 1 : k
+            img{i} = A(:, :, IDX(i));
+        end
+        
+        % downsampling
+        factor = 2;
+        for i = 1 : k
+            img{i} = conv2(img{i}, ones(factor) / factor ^ 2, 'same');
+            img{i} = img{i}(1 : factor : end, 1 : factor : end);
+        end
+        
+        % set optimization parameters
+        optPara.theta = 1;
+        optPara.maxIter = 2000;
+        optPara.tol = 1e-3;
+        optPara.outerIter = [25 2];
+        optPara.mu = 2e-1;
+        optPara.nu_factor = [0.9 1];
+        optPara.bc = 'neumann';
+        optPara.doPlots = false;
+        
+    otherwise
+        error('No such dataset!');
+        
 end
-
-% set optimization parameters
-optPara.theta = 1;
-optPara.maxIter = 2000;
-optPara.tol = 1e-3;
-optPara.outerIter = 20;
-optPara.mu = 1.25e-1;
-optPara.nu_factor = 0.9;
-optPara.bc = 'linear';
-optPara.doPlots = true;
 
 % call registration routine
 tic;
@@ -69,13 +117,13 @@ colormap gray(256);
 while true
     for i = 1 : k
         subplot(1, 3, 1);
-        imshow(img{i}, [0 1], 'InitialMagnification', 'fit');
+        imshow(img{i}, [], 'InitialMagnification', 'fit');
         title(sprintf('input T_{%d}', i));
         subplot(1, 3, 2);
-        imshow(img_u{i}, [0 1], 'InitialMagnification', 'fit');
+        imshow(img_u{i}, [], 'InitialMagnification', 'fit');
         title(sprintf('output T_{%d}(u_{%d})', i, i));
         subplot(1, 3, 3);
-        imshow(L{end, 1}(:, :, i), [0 1], 'InitialMagnification', 'fit');
+        imshow(L{end, 1}(:, :, i), [], 'InitialMagnification', 'fit');
         title(sprintf('output L_{%d}', i));
         waitforbuttonpress;
     end
