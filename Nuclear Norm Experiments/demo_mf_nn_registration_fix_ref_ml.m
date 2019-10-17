10c0db8d5d006880c75b03243c15dbc931936bc1
Roland Haase <roland.haase@mic.uni-luebeck.de>
2019-10-17 08:55:17 +0200

    parameter optimization + optical flow visualization

--- Nuclear Norm Experiments/demo_mf_nn_registration_fix_ref_ml.m
+++ Nuclear Norm Experiments/demo_mf_nn_registration_fix_ref_ml.m
@@ -9,10 +9,11 @@
 %           MIT Open Source License
 %--------------------------------------------------------------------------
 
-exp_begin();
-
 % demo script for mf_nn_registration_no_ref_ml.m
 clear all, close all, clc;
+
+% track experiment
+exp_begin();
 
 % normalization to [0, 1]
 normalize = @(y) (y - min(y(:))) / (max(y(:)) - min(y(:)));
@@ -20,7 +21,7 @@ normalize = @(y) (y - min(y(:))) / (max(y(:)) - min(y(:)));
 % select data set from {'BlinkingArrow', 'CarTruck', 'CrossingCars', ...
 %   'FlyingSnow', 'NightAndSnow', 'RainBlur', 'RainFlares', ...
 %   'ReflectingCar', 'ShadowOnTruck', 'SunFlare', 'WetAutobahn'}
-data_set = 'FlyingSnow';
+data_set = 'WetAutobahn';
 data_path = sprintf('../Data/ChallengingSequences/%s/sequence', data_set);
 
 switch data_set
@@ -44,8 +45,8 @@ switch data_set
         optPara.maxIter = 2000;
         optPara.tol = 1e-3;
         optPara.outerIter = [20 1];
-        optPara.mu = 5e-2;
-        optPara.nu_factor = [0.95 1];
+        optPara.mu = 7.5e-2;
+        optPara.nu_factor = [0.95 0.95];
         optPara.bc = 'neumann';
         optPara.doPlots = true;
         
@@ -98,13 +99,41 @@ switch data_set
         optPara.maxIter = 2000;
         optPara.tol = 1e-3;
         optPara.outerIter = [16 2];
-        optPara.mu = 4.5e-2;
-        optPara.nu_factor = [0.95 0.95];
+        optPara.mu = 7.5e-2;
+        optPara.nu_factor = [0.975 0.975];
+        optPara.bc = 'neumann';
+        optPara.doPlots = false;
+        
+        % select reference
+        ref_idx = 3 - IDX(1) + 1;
+        
+    case 'WetAutobahn'
+        
+        % load + downsample images
+        IDX = 16 : 25;
+        k = numel(IDX);
+        img = cell(1, k);
+        factor = 2;
+        for i = 1 : k
+            img{i} = normalize(double( ...
+                sub_imread(sprintf('%s/%06d_1.pgm', data_path, IDX(i)))));
+            img{i} = conv2(img{i}, ones(factor) / factor ^ 2, 'same');
+            img{i} = img{i}(1 : factor : end, 1 : factor : end);
+        end
+        
+        % optimization parameters
+        optPara.theta = 1;
+        optPara.maxIter = 2000;
+        optPara.tol = 1e-3;
+        optPara.weighting = 0.5;
+        optPara.outerIter = [20 2];
+        optPara.mu = 1e-1;
+        optPara.nu_factor = [0.975 0.975];
         optPara.bc = 'neumann';
         optPara.doPlots = true;
         
         % select reference
-        ref_idx = 3 - IDX(1) + 1;
+        ref_idx = 20 - IDX(1) + 1;
         
     otherwise
         
@@ -117,16 +146,17 @@ tic;
 [u, L, SV_history] = mf_nn_registration_fix_ref_ml(img, ref_idx, optPara);
 toc;
 
-%% display results
-
 img_u = cell(k, 1);
 for i = 1 : k
     img_u{i} = evaluate_displacement(img{i}, [1, 1], ...
         u{end, optPara.outerIter(2)}(:, :, i));
 end
 
-exp_save('data');
+% end tracking
 exp_end();
+exp_save('data');
+
+%% display results
 
 % input, output and low rank components in comparison
 figure;
@@ -140,8 +170,29 @@ while true
         imshow(img_u{i}, [], 'InitialMagnification', 'fit');
         title(sprintf('output T_{%d}(u_{%d})', i, i));
         subplot(1, 3, 3);
-        imshow(L{end, 1}(:, :, i), [], 'InitialMagnification', 'fit');
+        imshow(L{end, optPara.outerIter(2)}(:, :, i), [], 'InitialMagnification', 'fit');
         title(sprintf('output L_{%d}', i));
+        waitforbuttonpress;
+    end
+end
+
+% optical flow visualization
+for i = 1 : k
+    uStar{i} = ...
+        reshape(u{end, optPara.outerIter(2)}(:, :, i), [size(img{1}), 2]);
+    of{i} = flow_visualization(uStar{i}(:, :, 1), uStar{i}(:, :, 2));
+    overlay{i} = createOverlayImage(img{i}, of{i});
+end
+
+figure;
+while true
+    for i = 1 : k
+        subplot(1, 2, 1);
+        imshow(of{i}, 'InitialMagnification', 'fit');
+        title(sprintf('optical flow #%d', i));
+        subplot(1, 2, 2);
+        imshow(overlay{i}, 'InitialMagnification', 'fit');
+        title(sprintf('overlay #%d', i));
         waitforbuttonpress;
     end
 end
