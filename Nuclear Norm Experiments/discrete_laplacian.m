c0963b29536586f0967221b644ddcd75e2f50596
Roland Haase <roland.haase@hotmail.de>
2020-07-31 16:55:40 +0200

    fixed scaling bug

--- Nuclear Norm Experiments/discrete_laplacian.m
+++ Nuclear Norm Experiments/discrete_laplacian.m
@@ -20,15 +20,15 @@ function L = discrete_laplacian(m, n, h_grid, k)
 
 % x-derivative
 e_x = ones(m, 1);
-D_xx = (1 / h_grid(1)) * spdiags([e_x, (-2) * e_x, e_x], -1 : 1, m, m);
-D_xx(1, 1) = (-1) / h_grid(1);
-D_xx(end, end) = (-1) / h_grid(1);
+D_xx = (1 / h_grid(1)) ^ 2 * spdiags([e_x, (-2) * e_x, e_x], -1 : 1, m, m);
+D_xx(1, 1) = (-1) / h_grid(1) ^ 2;
+D_xx(end, end) = (-1) / h_grid(1) ^ 2;
 
 % y-derivative
 e_y = ones(n, 1);
-D_yy = (1 / h_grid(2)) * spdiags([e_y, (-2) * e_y, e_y], -1 : 1, n, n);
-D_yy(1, 1) = (-1) / h_grid(2);
-D_yy(end, end) = (-1) / h_grid(2);
+D_yy = (1 / h_grid(2)) ^ 2 * spdiags([e_y, (-2) * e_y, e_y], -1 : 1, n, n);
+D_yy(1, 1) = (-1) / h_grid(2) ^ 2;
+D_yy(end, end) = (-1) / h_grid(2) ^ 2;
 
 % construct laplacian, extend to 2*k grids (one per coordinate direction)
 L = kron(speye(2 * k), kron(speye(n), D_xx) + kron(D_yy, speye(m)));
